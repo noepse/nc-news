f2b6d6fc0cd489fd3c7b9e612bde6ced1b95155d
simmi <22905246+noepse@users.noreply.github.com>
2024-03-13 16:11:47 +0000

    add form components

--- src/pages/submit_article.jsx
+++ src/pages/submit_article.jsx
@@ -1,7 +1,57 @@
+import { useState, useContext, useEffect } from 'react';
+import { FormControl } from '@mui/material';
+
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+import { CurrentUserContext } from '../contexts/CurrentUser';
+
+import { getTopics } from '../utils/api';
+
 export default function Submit_Article(){
+    const [topics, setTopics] = useState([])
+    const [topic, setTopic] = useState('');
+    const { currentUser } = useContext(CurrentUserContext);
+
+    useEffect(()=>{
+        getTopics().then((topics)=>{
+            setTopics(topics)
+        })
+    }, [])
+
+    const handleChange = (event) => {
+        setTopic(event.target.value);
+      };
+
     return (
         <section id="submit">
-            <p>Coming soon!</p>
+            <h2>Post an article </h2>
+            <p>as {currentUser.username}</p>
+            <FormControl>
+            <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="topic-label">Topic</InputLabel>
+        <Select
+          labelId="topic-label"
+          id="topic-select"
+          value={topic}
+          label="Topic"
+          onChange={handleChange}
+        >
+            {topics.map((topic)=>{
+                return <MenuItem value={topic.slug}>{topic.slug}</MenuItem>
+            })}
+        </Select>
+      </FormControl>
+    </Box>
+<TextField id="title" label="Title" variant="outlined" />
+<TextField id="body" label="Body" variant="outlined" multiline rows={4}/>
+<Button variant="contained">Submit</Button>
+</FormControl>
             </section>
         )
 }
