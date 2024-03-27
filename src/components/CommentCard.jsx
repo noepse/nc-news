9f28814a7da699478ae10313709814d94188f296
simmi <22905246+noepse@users.noreply.github.com>
2024-03-27 13:45:12 +0000

    add comment votes

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,7 +1,52 @@
-import ReactDOM from 'react-dom'
+import { useState } from 'react';
+import IconButton from '@mui/material/IconButton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThumbsUp as solidThumbsUp, faThumbsDown as solidThumbsDown } from '@fortawesome/free-solid-svg-icons'
+import { faThumbsUp as outlineThumbsUp, faThumbsDown as outlineThumbsDown } from '@fortawesome/free-regular-svg-icons'
+
+import { patchVotesOnCommentById } from '../utils/api';
 
 export default function CommentCard(props){
     const {comment} = props
+
+    const [currentVotes, setCurrentVotes] = useState(comment.votes)
+    const [isUpVoted, setIsUpVoted] = useState(false);
+    const [isDownVoted, setIsDownVoted] = useState(false);
+    
+
+    function handleVote(vote){
+        let votes = 0
+        if (vote === 'upvote'){ 
+            votes = 1
+            if (isUpVoted){ 
+                votes = -1 
+            }
+            if (isDownVoted){
+                votes = 2
+            }
+            setIsUpVoted(!isUpVoted) 
+            setIsDownVoted(false) 
+        }
+        if (vote === 'downvote'){
+            votes = -1
+            if (isDownVoted){
+                votes = 1 
+            }
+            if (isUpVoted){
+                votes = -2
+            }
+            setIsDownVoted(!isDownVoted)
+            setIsUpVoted(false)
+        }
+
+        setCurrentVotes(currentVotes + votes)
+
+        patchVotesOnCommentById(comment.comment_id, votes)
+        .then((commentData)=>{
+            console.log('voted')
+        })
+    }
+
     return (
         <div className="comment">
         <div className = "commentHeader">
@@ -9,7 +54,19 @@ export default function CommentCard(props){
         {props.children}
         </div>
         <p>{comment.body}</p>
-        <span>{comment.votes} votes</span>
+        <div>
+        <IconButton aria-label="upvote article" onClick = {()=>{
+            handleVote('upvote')
+        }} >
+        <FontAwesomeIcon className="voteBtn" icon={isUpVoted? solidThumbsUp : outlineThumbsUp} size='sm'/> 
+      </IconButton>
+        <span>{currentVotes} votes</span>
+        <IconButton aria-label="downvote article" onClick = {(event)=>{
+            handleVote('downvote')
+        }} >
+        <FontAwesomeIcon className = "voteBtn" icon={isDownVoted? solidThumbsDown : outlineThumbsDown} size='sm'/> 
+      </IconButton>
+      </div>
         </div>
     )
 }
