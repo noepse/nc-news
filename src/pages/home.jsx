8fc4d301e5219b12ed9973fa89f7ae7857dde5b8
simmi <22905246+noepse@users.noreply.github.com>
2024-03-28 16:53:46 +0000

    add loading backdrop

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,16 +1,32 @@
 import Topics from '../components/Topics'
 import Articles from '../components/Articles'
 import { useState } from 'react'
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+import HashLoader from "react-spinners/HashLoader";
 
 export default function Home(props){
+    const {setCurrentArticle} = props
+
     const [articles, setArticles] = useState(null)
     const [isLoadingArticles, setIsLoadingArticles] = useState(true)
-    const {setCurrentArticle} = props
 
     return (
         <>
+              {isLoadingArticles ? <Backdrop
+        sx={{ color: '#fff', flexDirection: 'column', gap: '1em', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={isLoadingArticles}
+      > 
+        <h1>Loading articles ... </h1>
+        <HashLoader
+        color={'#fff'}
+        loading={isLoadingArticles}
+        size={70}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+      </Backdrop> : null}
         <Topics setArticles={setArticles} setIsLoadingArticles={setIsLoadingArticles}/>
-
         <Articles setCurrentArticle={setCurrentArticle} articles={articles} setArticles={setArticles}  isLoadingArticles={isLoadingArticles} setIsLoadingArticles={setIsLoadingArticles} />
         </>
 
